8e34d02fa77fe9feb7d83bd454d65eac89fd9b5a
seokjun01 <suckjungod@naver.com>
2025-10-19 16:43:51 +0900

    chore : 프로젝트 생성 및 환경세팅

---
+++ src/main/java/tave/crezipsa/crezipsa/CrezipsaApplication.java
@@ -0,0 +1,13 @@
+package tave.crezipsa.crezipsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrezipsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrezipsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tave/crezipsa/crezipsa/CrezipsaApplicationTests.java
@@ -0,0 +1,13 @@
+package tave.crezipsa.crezipsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrezipsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
