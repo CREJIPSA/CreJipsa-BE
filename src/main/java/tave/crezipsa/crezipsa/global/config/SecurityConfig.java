bf9b534094dbfbb8678fd87779fb7bc7fe430e64
seokjun01 <suckjungod@naver.com>
2025-10-27 08:48:11 +0900

    chore : Spring Security 및 스웨거 파일 생성

---
+++ src/main/java/tave/crezipsa/crezipsa/global/config/SecurityConfig.java
@@ -0,0 +1,5 @@
+package tave.crezipsa.crezipsa.global.config;
+// 인증 인가 및 설정 하는곳
+public class SecurityConfig {
+
+}
---
+++ src/main/java/tave/crezipsa/crezipsa/global/config/SwaggerConfig.java
@@ -0,0 +1,4 @@
+package tave.crezipsa.crezipsa.global.config;
+// 스웨거 api 문서화 관련 설정
+public class SwaggerConfig {
+}
