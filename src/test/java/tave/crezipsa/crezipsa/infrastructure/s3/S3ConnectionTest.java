5df41fa8daf287e94050e445148d48d239c23d29
seokjun01 <suckjungod@naver.com>
2025-11-07 00:09:29 +0900

    test : S3버킷 연결 테스트 코드 작성

--- src/test/java/tave/crezipsa/crezipsa/infrastructure/s3/S3ConnectionTest.java
+++ src/test/java/tave/crezipsa/crezipsa/infrastructure/s3/S3ConnectionTest.java
@@ -1,4 +1,28 @@
 package tave.crezipsa.crezipsa.infrastructure.s3;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+
+@SpringBootTest
 public class S3ConnectionTest {
+
+	@Autowired
+	private S3Client s3Client;
+
+	@Test
+	void listBuckets() {
+		ListObjectsV2Response result = s3Client.listObjectsV2(
+			ListObjectsV2Request.builder()
+				.bucket(System.getenv("AWS_S3_BUCKET"))
+				.build()
+		);
+		System.out.println(" 연결 성공 버킷 내 객체 수: " + result.contents().size());
+
+	}
 }
