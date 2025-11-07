b52eaa5809d3ec4b3a58ffbd40c30d4d2ad966db
seokjun01 <suckjungod@naver.com>
2025-11-08 03:01:34 +0900

    feat : 업로드/다운로드/삭제 api 생성

---
+++ src/main/java/tave/crezipsa/crezipsa/presentation/file/controller/FileController.java
@@ -0,0 +1,44 @@
+package tave.crezipsa.crezipsa.presentation.file.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import tave.crezipsa.crezipsa.application.file.usecase.FileUseCase;
+import tave.crezipsa.crezipsa.global.common.dto.GlobalResponseDto;
+import tave.crezipsa.crezipsa.presentation.file.response.DownloadUrlResponse;
+import tave.crezipsa.crezipsa.presentation.file.response.UploadUrlResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/files")
+public class FileController {
+
+	private final FileUseCase fileUseCase;
+
+	@PostMapping("/upload")
+	public GlobalResponseDto<UploadUrlResponse> createUploadUrl(
+		@RequestParam String fileName,
+		@RequestParam String contentType
+	) {
+		return GlobalResponseDto.success(fileUseCase.generateUploadUrl(fileName, contentType));
+	}
+
+	@GetMapping("/download-url")
+	public GlobalResponseDto<DownloadUrlResponse> createDownloadUrl(
+		@RequestParam String fileName
+	) {
+		return GlobalResponseDto.success(fileUseCase.generateDownloadUrl(fileName));
+	}
+
+	@DeleteMapping
+	public GlobalResponseDto<Void> deleteFile(@RequestParam String fileName) {
+		fileUseCase.deleteFile(fileName);
+		return GlobalResponseDto.success(null);
+	}
+
+}
