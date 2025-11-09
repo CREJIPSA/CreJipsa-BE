4ad83e22b5a19133a50fb9faafceee05c142636b
seokjun01 <suckjungod@naver.com>
2025-11-09 22:56:01 +0900

    fix : 언제든 조회가능한 정적 url 생성 로직 추가

--- src/main/java/tave/crezipsa/crezipsa/infrastructure/s3/S3FileStorage.java
+++ src/main/java/tave/crezipsa/crezipsa/infrastructure/s3/S3FileStorage.java
@@ -1,28 +1,23 @@
 package tave.crezipsa.crezipsa.infrastructure.s3;
 
+import java.net.URL;
 import java.time.Duration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
-
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
-import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 import tave.crezipsa.crezipsa.domain.file.port.FileStoragePort;
 
-
-
 @Component
 @RequiredArgsConstructor
-public class S3FileStorage  implements FileStoragePort {
+public class S3FileStorage implements FileStoragePort {
 
 	private final S3Client s3Client;
 	private final S3Presigner s3Presigner;
@@ -30,16 +25,20 @@ public class S3FileStorage  implements FileStoragePort {
 	@Value("${infra.s3.bucket}")
 	private String bucket;
 
+	@Value("${infra.s3.region}")
+	private String region;
+
 	@Override
 	public String generateUploadUrl(String fileName, String contentType) {
+
 		PutObjectRequest put = PutObjectRequest.builder()
 			.bucket(bucket)
 			.key(fileName)
 			.contentType(contentType)
+			.acl("public-read")
 			.build();
 
 		PutObjectPresignRequest presign = PutObjectPresignRequest.builder()
-			// 필요 시 만료시간 조정
 			.signatureDuration(Duration.ofMinutes(10))
 			.putObjectRequest(put)
 			.build();
@@ -48,20 +47,11 @@ public class S3FileStorage  implements FileStoragePort {
 		return result.url().toString();
 	}
 
+
 	@Override
 	public String generateDownloadUrl(String fileName) {
-		GetObjectRequest get = GetObjectRequest.builder()
-			.bucket(bucket)
-			.key(fileName)
-			.build();
-
-		GetObjectPresignRequest presign = GetObjectPresignRequest.builder()
-			.signatureDuration(Duration.ofMinutes(10))
-			.getObjectRequest(get)
-			.build();
-
-		PresignedGetObjectRequest result = s3Presigner.presignGetObject(presign);
-		return result.url().toString();
+		// fileUrl은 항상 접근 가능한 S3 정적 URL로 대체
+		return String.format("https://%s.s3.%s.amazonaws.com/%s", bucket, region, fileName);
 	}
 
 	@Override
@@ -72,5 +62,4 @@ public class S3FileStorage  implements FileStoragePort {
 			.build();
 		s3Client.deleteObject(req);
 	}
-
 }
