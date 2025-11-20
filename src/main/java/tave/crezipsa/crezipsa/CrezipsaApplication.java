7338e94f4b1beb9a81bf8ea5def94fbbfe2f6d48
seokjun01 <suckjungod@naver.com>
2025-11-20 21:40:56 +0900

    add : BaseEntity 활성화를 위한 어노테이션 추가

--- src/main/java/tave/crezipsa/crezipsa/CrezipsaApplication.java
+++ src/main/java/tave/crezipsa/crezipsa/CrezipsaApplication.java
@@ -2,8 +2,10 @@ package tave.crezipsa.crezipsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CrezipsaApplication {
 
 	public static void main(String[] args) {
