16c43fafb905e92b646863821bd67ad62d1f0006
seokjun01 <suckjungod@naver.com>
2025-11-22 01:45:29 +0900

    add : 좋아요 관련 메소드 추가

--- src/main/java/tave/crezipsa/crezipsa/domain/community/repository/LikeRepository.java
+++ src/main/java/tave/crezipsa/crezipsa/domain/community/repository/LikeRepository.java
@@ -12,5 +12,7 @@ public interface LikeRepository {
 	Optional<Like> findById(LikeId likedId);
 	void delete(Like like);
 	boolean existsById(LikeId likedId);
-
+	boolean existsByUserIdAndCommunityId(Long userId, Long communityId);
+	void deleteByUserIdAndCommunityId(Long userId, Long communityId);
+	long countByCommunityId(Long communityId);
 }
