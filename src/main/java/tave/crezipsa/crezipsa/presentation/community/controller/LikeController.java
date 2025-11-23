9c25adc9b87f36deef9ad0f00d743dc8fc2d5ede
seokjun01 <suckjungod@naver.com>
2025-11-23 11:47:40 +0900

    fix : 인증/인가 미적용 api(테스트용)

--- src/main/java/tave/crezipsa/crezipsa/presentation/community/controller/LikeController.java
+++ src/main/java/tave/crezipsa/crezipsa/presentation/community/controller/LikeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -23,7 +24,7 @@ import tave.crezipsa.crezipsa.global.common.dto.GlobalResponseDto;
 public class LikeController {
 
 	private final LikeUseCase likeUseCase;
-
+/*
 	@PostMapping("/{communityId}")
 	public GlobalResponseDto<Void> like(@AuthenticationPrincipal User user, @PathVariable Long communityId) {
 		likeUseCase.like(user.getUserId(), communityId);
@@ -49,6 +50,31 @@ public class LikeController {
 		var slice = likeUseCase.getMyLikedCommunities(user.getUserId(), pageable);
 		return GlobalResponseDto.success(slice.getContent());
 	}
+*/
+    @PostMapping("/{communityId}")
+	public GlobalResponseDto<Void> like(@RequestParam Long userId, @PathVariable Long communityId) {
+		likeUseCase.like(userId, communityId);
+		return GlobalResponseDto.success();
+	}
 
+	@DeleteMapping("/{communityId}")
+	public GlobalResponseDto<Void> unlike(@RequestParam Long userId, @PathVariable Long communityId) {
+		likeUseCase.unlike(userId, communityId);
+		return GlobalResponseDto.success();
+	}
+
+	@GetMapping("/{communityId}")
+	public GlobalResponseDto<Long> getLikeCount(@PathVariable Long communityId) {
+		return GlobalResponseDto.success(likeUseCase.getLikeCount(communityId));
+	}
+
+	@GetMapping("/me")
+	public GlobalResponseDto<List<MyLikedCommunityResponse>> getMyLikedCommunities(
+		@RequestParam Long userId,
+		Pageable pageable
+	) {
+		var slice = likeUseCase.getMyLikedCommunities(userId, pageable);
+		return GlobalResponseDto.success(slice.getContent());
+	}
 
 }
