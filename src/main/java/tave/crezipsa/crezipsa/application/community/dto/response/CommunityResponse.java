c7f961768b78e1b89af01d23646701265eceaf03
seokjun01 <suckjungod@naver.com>
2025-11-26 18:12:42 +0900

    refactor : 반환 DTO record로 통일

--- src/main/java/tave/crezipsa/crezipsa/application/community/dto/response/CommunityResponse.java
+++ src/main/java/tave/crezipsa/crezipsa/application/community/dto/response/CommunityResponse.java
@@ -3,36 +3,30 @@ package tave.crezipsa.crezipsa.application.community.dto.response;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 import tave.crezipsa.crezipsa.domain.community.domain.Community;
 import tave.crezipsa.crezipsa.domain.community.domain.CommunityField;
 
-@Getter
-@AllArgsConstructor
-@Builder
-public class CommunityResponse {
-
-	private Long communityId;
-	private String title;
-	private String content;
-	private CommunityField field;
-	private List<String> imageUrls;
-	private Long writerId;
-	private Long likeCount;
-	private LocalDateTime createdAt;
+public record CommunityResponse(
+	Long communityId,
+	String title,
+	String content,
+	CommunityField field,
+	List<String> imageUrls,
+	Long writerId,
+	Long likeCount,
+	LocalDateTime createdAt
+) {
 
 	public static CommunityResponse from(Community community) {
-		return CommunityResponse.builder()
-			.communityId(community.getCommunityId())
-			.title(community.getTitle())
-			.content(community.getContent())
-			.field(community.getField())
-			.imageUrls(community.getImageUrls())
-			.writerId(community.getWriterId())
-			.likeCount(community.getLikeCount())
-			.createdAt(community.getCreatedAt())
-			.build();
+		return new CommunityResponse(
+			community.getCommunityId(),
+			community.getTitle(),
+			community.getContent(),
+			community.getField(),
+			community.getImageUrls(),
+			community.getWriterId(),
+			community.getLikeCount(),
+			community.getCreatedAt()
+		);
 	}
 }
