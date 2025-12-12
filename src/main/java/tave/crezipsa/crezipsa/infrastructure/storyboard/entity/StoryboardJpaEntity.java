131f4f08616d9415f2a51ac698a28ef4b707f886
seokjun01 <suckjungod@naver.com>
2025-12-12 15:22:20 +0900

    feat : storyboard JPA Entity 구현

---
+++ src/main/java/tave/crezipsa/crezipsa/infrastructure/storyboard/entity/StoryboardJpaEntity.java
@@ -0,0 +1,34 @@
+package tave.crezipsa.crezipsa.infrastructure.storyboard.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "story_board")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class StoryboardJpaEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long storyBoardId;
+
+	private Long userId;
+
+	private String storyboardTitle;
+
+	@Column(columnDefinition = "TEXT")
+	private String content;
+
+}
