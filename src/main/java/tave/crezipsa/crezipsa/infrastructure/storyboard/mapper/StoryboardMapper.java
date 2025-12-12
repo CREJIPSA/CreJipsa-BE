66c6d6a97c9e41a7c8f8d39d076d571e7f67a50c
seokjun01 <suckjungod@naver.com>
2025-12-12 15:25:57 +0900

    storyboardJPAEntity -> storyboardEntity 매퍼 구현

---
+++ src/main/java/tave/crezipsa/crezipsa/infrastructure/storyboard/mapper/StoryboardMapper.java
@@ -0,0 +1,17 @@
+package tave.crezipsa.crezipsa.infrastructure.storyboard.mapper;
+
+import tave.crezipsa.crezipsa.domain.storyboard.entity.Storyboard;
+import tave.crezipsa.crezipsa.infrastructure.storyboard.entity.StoryboardJpaEntity;
+
+public class StoryboardMapper {
+
+	public static Storyboard toDomain(StoryboardJpaEntity e) {
+		return Storyboard.builder()
+			.id(e.getStoryBoardId())
+			.userId(e.getUserId())
+			.title(e.getStoryboardTitle())
+			.content(e.getContent())
+			.build();
+	}
+
+}
