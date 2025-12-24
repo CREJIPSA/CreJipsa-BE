4fb0d7d271746c0bf7c54deff20ad83c7a0ddeaf
seokjun01 <suckjungod@naver.com>
2025-12-24 23:49:10 +0900

    refactor : 게시글 수정에도 요구사항 반영

--- src/main/java/tave/crezipsa/crezipsa/domain/community/domain/Community.java
+++ src/main/java/tave/crezipsa/crezipsa/domain/community/domain/Community.java
@@ -59,7 +59,10 @@ public class Community extends BaseEntity {
 	public void update(String title, String content, List<String> imageUrls) {
 		if (title != null) this.title = title;
 		if (content != null) this.content = content;
-		if (imageUrls != null) this.imageUrls = imageUrls;
+		if (imageUrls != null) {
+			validateTipField(this.field, imageUrls);
+			this.imageUrls = imageUrls;
+		}
 	}
 
 	public static Community create(String title, String content, CommunityField field, List<String> imageUrls, Long writerId) {
