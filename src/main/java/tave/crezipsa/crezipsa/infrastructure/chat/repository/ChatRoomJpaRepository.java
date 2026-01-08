c799e1857d59ac83e4a0a9559cd13dd6e31f0b3e
seokjun01 <suckjungod@naver.com>
2026-01-08 16:34:35 +0900

    add : 레포지토리 메소드 추가

--- src/main/java/tave/crezipsa/crezipsa/infrastructure/chat/repository/ChatRoomJpaRepository.java
+++ src/main/java/tave/crezipsa/crezipsa/infrastructure/chat/repository/ChatRoomJpaRepository.java
@@ -1,9 +1,14 @@
 package tave.crezipsa.crezipsa.infrastructure.chat.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tave.crezipsa.crezipsa.infrastructure.chat.entity.ChatRoomJpaEntity;
 
 public interface ChatRoomJpaRepository extends JpaRepository<ChatRoomJpaEntity, Long> {
 
+	List<ChatRoomJpaEntity> findByUserId(Long userId);
+	Optional<ChatRoomJpaEntity> findByIdAndUserId(Long id, Long userId);
 }
