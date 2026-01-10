08285201b4aa85a541a18827afcca21377cd2f83
dlwlsgmlgh <jinhee020928@naver.com>
2026-01-10 22:23:42 +0900

    feat: 트렌드 조회/저장 기능 개발

--- src/main/java/tave/crezipsa/crezipsa/infrastructure/trend/TrendAdapter.java
+++ src/main/java/tave/crezipsa/crezipsa/infrastructure/trend/TrendAdapter.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.PlatformTransactionManager;
 import tave.crezipsa.crezipsa.application.trend.port.TrendQueryPort;
+import tave.crezipsa.crezipsa.domain.trend.entity.command.TrendCommand;
 import tave.crezipsa.crezipsa.domain.user.enums.Platform;
 
 import java.util.List;
@@ -15,28 +16,152 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TrendAdapter implements TrendQueryPort {
 
-    private final @Qualifier("analyticsJdbc") NamedParameterJdbcTemplate jdbc;
+    private final @Qualifier("analyticsJdbc") NamedParameterJdbcTemplate analyticsJdbc;
+    private final @Qualifier("mainJdbc") NamedParameterJdbcTemplate mainJdbc;
 
     @Override
-    public List<TrendRow> findTopKeywordsByPlatform(String platform) {
+    public List<TrendRow> findTopKeywordsByPlatform(String platform, String category) {
 
         String sql = """
-            SELECT id, `rank` AS keyword_rank, keyword, platform
+            SELECT id, `rank` AS keyword_rank, keyword, platform, category_tag
             FROM analytics_top_keywords
-            WHERE platform = :platform
-            ORDER BY keyword_rank ASC
+            WHERE platform = :platform 
         """;
 
         MapSqlParameterSource params = new MapSqlParameterSource()
-                .addValue("platform", platform);
+                .addValue("platform", platform)
+                .addValue("limit", 4);
 
-        List<TrendRow> rows = jdbc.query(sql, params, (rs, rowNum) -> new TrendRow(
+        if (category != null && !category.isBlank()) {
+            sql += " AND category = :category";
+            params.addValue("category", category);
+        }
+
+        sql += " ORDER BY keyword_rank DESC LIMIT :limit";
+
+        List<TrendRow> rows = analyticsJdbc.query(sql, params, (rs, rowNum) -> new TrendRow(
                 rs.getLong("id"),
                 rs.getInt("keyword_rank"),
                 rs.getString("keyword"),
-                rs.getString("platform")
+                rs.getString("platform"),
+                rs.getString("category_tag")
         ));
 
         return rows;
     }
+
+    @Override
+    public TrendDetailWithUrls findSelectedKeywordDetialBytrendId(long trendId) {
+
+        String keywordSql = """
+            SELECT id, `rank` AS keyword_rank, keyword, platform, frequency
+            FROM analytics_top_keywords
+            WHERE id = :id
+        """;
+
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("id", trendId)
+                .addValue("limit",5);
+
+        TrendDetailRow detailRow = analyticsJdbc.queryForObject(keywordSql, params,
+                (rs, rowNum) -> new TrendDetailRow(
+                        rs.getLong("id"),
+                        rs.getString("platform"),
+                        rs.getInt("keyword_rank"),
+                        rs.getString("keyword"),
+                        rs.getInt("frequency")
+                )
+        );
+
+        params.addValue("keyword", "%" + detailRow.keyword() + "%");
+
+        String urlSql = """
+            SELECT video_url, title
+            FROM URLTABLE
+            WHERE title LIKE :keyword
+            ORDER BY created_at DESC
+            LIMIT :limit
+        """;
+        // 검색 where 쿼리 후보: REPLACE(title, ' ', '') LIKE CONCAT('%', REPLACE(:keyword, ' ', ''), '%')
+
+        List<TrendUrlRow> urlRows = analyticsJdbc.query(urlSql, params, (rs, rowNum) -> new TrendUrlRow(
+                rs.getString(1),
+                rs.getString(2),
+              0  // rs.getInt(3) 아직 db에 카운트 필드가 없어 임의 값 설정
+        ));
+
+        return new TrendDetailWithUrls(detailRow, urlRows);
+    }
+
+    @Override
+    public void saveTrend(long userId, TrendCommand trendCommand) {
+
+        String Sql = """
+           INSERT INTO keyword_storaged (user_id, keyword_id_from_analytics, keyword, category)
+                       VALUES (:userId, :keywordIdFromAnalytics, :keyword, :category)    
+        """;
+
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("userId", userId)
+                .addValue("keywordIdFromAnalytics",trendCommand.keywordId())
+                .addValue("keyword",trendCommand.keyword())
+                .addValue("category",trendCommand.category());
+
+        mainJdbc.update(Sql, params);
+    }
+
+    @Override
+    public TrendWithUrls findKeywordByKeyword(String keyword) {
+
+        String keywordSql = """
+           WITH filtered AS (
+                            SELECT keyword_id, keyword, category_id
+                            FROM raw_keywords
+                            WHERE keyword LIKE :keyword
+                        ),
+                        dedup AS (
+                            SELECT
+                                keyword_id,
+                                keyword,
+                                category_id,
+                                ROW_NUMBER() OVER (
+                                    PARTITION BY keyword
+                                    ORDER BY keyword_id DESC
+                                ) AS rn
+                            FROM filtered
+                        )
+                        SELECT keyword_id, keyword, category_id
+                        FROM dedup
+                        WHERE rn = 1
+                        ORDER BY keyword_id DESC
+                        LIMIT :limit;
+                        
+        """;
+
+        String urlSql = """
+            SELECT video_url, title, view_count
+            FROM URLTABLE
+            WHERE title LIKE :keyword
+            ORDER BY created_at DESC
+            LIMIT :limit
+        """;
+
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("keyword", "%" + keyword + "%")
+                .addValue("limit",50);
+
+        List<TrendRow> trends = analyticsJdbc.query(keywordSql, params, (rs, rowNum) -> new TrendRow(
+                rs.getLong("keyword_id"),
+                rs.getString("keyword"),
+                rs.getString("category_id")));
+
+        List<TrendUrlRow> urls = analyticsJdbc.query(urlSql, params, (rs, rowNum)  -> new TrendUrlRow(
+                rs.getString("video_url"),
+                rs.getString("title"),
+                rs.getInt("view_count")));
+
+        return new TrendWithUrls(trends, urls);
+
+    }
+
 }
