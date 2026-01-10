f6ad3284ce63b0c4984459c65d54b0cedeae0323
dlwlsgmlgh <jinhee020928@naver.com>
2026-01-10 21:16:06 +0900

    add: url response 정의

---
+++ src/main/java/tave/crezipsa/crezipsa/application/trend/dto/response/TrendUrlResponse.java
@@ -0,0 +1,18 @@
+package tave.crezipsa.crezipsa.application.trend.dto.response;
+
+import tave.crezipsa.crezipsa.infrastructure.trend.TrendDetailRow;
+import tave.crezipsa.crezipsa.infrastructure.trend.TrendUrlRow;
+
+public record TrendUrlResponse(
+        String title,
+        String url,
+        int viewCount
+) {
+    public static TrendUrlResponse  from(TrendUrlRow row) {
+        return new TrendUrlResponse(
+                row.title(),
+                row.url(),
+                row.viewCount()
+        );
+    }
+}
