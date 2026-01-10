87f7528c38cddda3af6d0862bcedfce1748e2222
seokjun01 <suckjungod@naver.com>
2026-01-10 16:28:22 +0900

    add : 채팅방 검색 기능 인터페이스에 정의

--- src/main/java/tave/crezipsa/crezipsa/domain/chat/port/ChatRoomRepositoryPort.java
+++ src/main/java/tave/crezipsa/crezipsa/domain/chat/port/ChatRoomRepositoryPort.java
@@ -10,6 +10,7 @@ public interface ChatRoomRepositoryPort {
 	ChatRoom save(ChatRoom chatRoom);
 	Optional<ChatRoom> findById(Long chatRoomId);
 	List<ChatRoom> findByUserId(Long userId);
+	List<ChatRoom> searchByTitle(Long userId, String keyword);
 	Optional<ChatRoom> findByIdAndUserId(Long chatRoomId, Long userId);
 	void changeTitle(Long chatRoomId, Long userId, String title);
 }
