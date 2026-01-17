ccfa8be46d3af394db811e59a0b8b8ce196bed67
seokjun01 <suckjungod@naver.com>
2026-01-17 23:46:01 +0900

    feat: 사용자 정보 반환용 DTO 생성

---
+++ src/main/java/tave/crezipsa/crezipsa/application/community/dto/response/WriterResponse.java
@@ -0,0 +1,19 @@
+package tave.crezipsa.crezipsa.application.community.dto.response;
+
+import tave.crezipsa.crezipsa.domain.user.entity.User;
+import tave.crezipsa.crezipsa.domain.user.enums.Platform;
+
+public record WriterResponse(
+	Long userId,
+	String nickName,
+	String profileImageUrl,
+	Platform mainPlatform
+) {
+	public static WriterResponse from(User user) {
+		return new WriterResponse(
+			user.getUserId(),
+			user.getNickName(),
+			user.getProfileImageUrl(),
+			user.getMainPlatform()
+		);
+}
