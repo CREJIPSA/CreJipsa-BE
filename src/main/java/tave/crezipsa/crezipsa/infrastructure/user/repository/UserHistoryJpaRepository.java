ecddb9586d412d68dad67f4cd35ea0456e08aba0
dlwlsgmlgh <jinhee020928@naver.com>
2026-01-19 01:39:06 +0900

    add: 검색기록 관련 메소드 추가

--- src/main/java/tave/crezipsa/crezipsa/infrastructure/user/repository/UserHistoryJpaRepository.java
+++ src/main/java/tave/crezipsa/crezipsa/infrastructure/user/repository/UserHistoryJpaRepository.java
@@ -3,5 +3,9 @@ package tave.crezipsa.crezipsa.infrastructure.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tave.crezipsa.crezipsa.domain.user.entity.UserHistory;
 
+import java.util.List;
+
 public interface UserHistoryJpaRepository extends JpaRepository<UserHistory, Long> {
+    List<UserHistory> findTop20ByUserIdOrderByHistoryIdDesc(Long userId);
+    void deleteByUserId(long userId);
 }
--- src/main/java/tave/crezipsa/crezipsa/infrastructure/user/repository/UserHistoryRespositoryImpl.java
+++ src/main/java/tave/crezipsa/crezipsa/infrastructure/user/repository/UserHistoryRespositoryImpl.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 import tave.crezipsa.crezipsa.domain.user.entity.UserHistory;
 import tave.crezipsa.crezipsa.domain.user.repository.UserHistoryRepository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class UserHistoryRespositoryImpl implements UserHistoryRepository {
@@ -14,5 +16,20 @@ public class UserHistoryRespositoryImpl implements UserHistoryRepository {
     @Override
     public void save(UserHistory userHistory) {
         userHistoryJpaRepository.save(userHistory);
+    }
+
+    @Override
+    public List<UserHistory> findByUserId(long userId) {
+        return userHistoryJpaRepository.findTop20ByUserIdOrderByHistoryIdDesc(userId);
+    }
+
+    @Override
+    public void deleteByHistoryId(long historyId) {
+        userHistoryJpaRepository.deleteById(historyId);
+    }
+
+    @Override
+    public void deleteByUserId(long userId) {
+        userHistoryJpaRepository.deleteByUserId(userId);
     }
 }
